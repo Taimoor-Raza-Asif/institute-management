b4721071be4e678b01896413b04cda7a447e8af8
Taimoor-Raza-Asif <rajataimoor1937@gmail.com>
2026-05-18 20:03:14 +0500

    Changed the salaryMailer

--- backend/utils/salaryMailer.js
+++ backend/utils/salaryMailer.js
@@ -644,7 +644,7 @@ Jamia Tul Mastwaar
 
   const { data, error } = await resend.emails.send({
     from: 'Accounts Department - Jamia Tul Mastwaar <onboarding@resend.dev>',
-    replyTo: 'payroll.jamiatulmastwaar@gmail.com',
+    replyTo: 'jamia.tul.mastwaar.edu@gmail.com',
     to: staffEmail,
     subject,
     text: plainText,
